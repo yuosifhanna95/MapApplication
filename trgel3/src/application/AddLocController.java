fc6852b7a23d99e1a9d89a19e6098457fdb3f24c
yuosifhanna95 <41512322+yuosifhanna95@users.noreply.github.com>
2019-05-26 00:34:47 +0300

    Most Updated Version 26.5.19 12:34

--- trgel3/src/application/AddLocController.java
+++ trgel3/src/application/AddLocController.java
@@ -1,17 +1,12 @@
 package application;
 
-
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.net.URL;
-import java.net.UnknownHostException;
-
-import javax.imageio.ImageIO;
-
 import java.awt.image.RenderedImage;
 import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
@@ -36,218 +31,308 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class AddLocController {
-	private Label[] labels= new Label[10];
+	private int Mode;
+	private Label[] labels = new Label[10];
 	private ImageView[] locations = new ImageView[10];
-	private Place[] Places= new Place[10];
-	private int Counter=0;
-	private int current=0;
+	private Place[] Places = new Place[10];
+	private ImagePlace[] ImagePlaces = new ImagePlace[10];
+	private ImagePlace CurImagePlace;
+	private int Counter = 0;
+	private int current = 0;
 
-    @FXML
-    private AnchorPane mainPane;
-    
-    @FXML
-    private ImageView MapImage;
+	@FXML
+	private Button btn_UpdateMap;
 
-    @FXML
-    private Label label_message;
-    
-    @FXML
-    private Button back;
+	@FXML
+	private AnchorPane mainPane;
 
-    @FXML
-    private Button btn_save;
+	@FXML
+	private ImageView MapImage;
 
-    @FXML
-    private Button btn_EditMode;
+	@FXML
+	private Label label_message;
 
-    @FXML
-    private Button btn_ViewMode;
+	@FXML
+	private Button back;
 
-    @FXML
-    private Button btn_AddLoc;
-    
-    @FXML
-    private TextField NewLocation;
-    
-    @FXML
+	@FXML
+	private Button btn_save;
+
+	@FXML
+	private Button btn_EditMode;
+
+	@FXML
+	private Button btn_ViewMode;
+
+	@FXML
+	private Button btn_AddLoc;
+
+	@FXML
+	private TextField NewLocation;
+
+	@FXML
 	private Button OK;
-    
-    @FXML
-    private ImageView raninImage;
-    
-    
-    @FXML
-    void AddLocation(ActionEvent event) {
-    	if(!NewLocation.getText().equals("") && Counter<10)
-    	{
-    		EventHandler<MouseEvent> OkEventHandler = new EventHandler<MouseEvent>() {
-    			
-    			@Override
-    			public void handle(MouseEvent event) {
-    				if(event.getEventType()==MouseEvent.MOUSE_CLICKED)
-    				{
-    					mainPane.getChildren().remove(OK);
-    					current=-1;
-    				}
-    				// TODO Auto-generated method stub
 
-    		    	//mainPane.getChildren().addAll(newLoc);
-    			}
-    		};
-    		current = Counter;
-        	Label label= new Label();
-        	OK = new Button();
-        	OK.setText("set");
-        	OK.addEventHandler(MouseEvent.MOUSE_CLICKED, OkEventHandler);
-        	label.setText(NewLocation.getText());
-        	label.setFont(new Font("Arial",20));
-        	Image im= new Image("File:loc.png");
-        	ImageView newLoc = new ImageView(im);
-        	newLoc.relocate(MapImage.getLayoutX(), MapImage.getLayoutY());
-        	label.relocate(MapImage.getLayoutX(), MapImage.getLayoutY());
-        	double aspect=0.75f;
-        	newLoc.setFitHeight(im.getHeight() * aspect);
-        	newLoc.setFitWidth(im.getWidth() * aspect);
-        	labels[Counter]=label;
-        	locations[Counter]=newLoc;
-        	Places[Counter]= new Place();
-        	mainPane.getChildren().addAll(locations[Counter]);
-        	mainPane.getChildren().addAll(labels[Counter]);
-        	mainPane.getChildren().addAll(OK);
-        	EventHandler<MouseEvent> mouseEvent = new EventHandler<MouseEvent>() {
-    			
-    			@Override
-    			public void handle(MouseEvent event) {
-    				if(current!=-1)
-    				{
-	    				if(event.getEventType()==MouseEvent.MOUSE_CLICKED || event.getEventType()==MouseEvent.MOUSE_DRAGGED)
-	    				{
-	    					locations[current].relocate(event.getSceneX()-(im.getWidth()/2)*aspect, event.getSceneY()-(im.getHeight()/2)*aspect);
-	    					labels[current].relocate(event.getSceneX()-(labels[current].getWidth()/2), event.getSceneY()-(labels[current].getHeight() +(im.getHeight()/2)*aspect));
-	    					OK.relocate(event.getSceneX()-(OK.getWidth()/2), event.getSceneY()+(OK.getHeight()));
-	    				}
-	    				// TODO Auto-generated method stub
+	@FXML
+	private ImageView raninImage;
 
-    				}
-    			}
-    		};
-        	mainPane.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent);
-        	mainPane.addEventHandler(MouseEvent.MOUSE_DRAGGED, mouseEvent);
-        	mainPane.addEventHandler(MouseEvent.MOUSE_RELEASED, mouseEvent);
-        	Counter++;
-    	}
-    	else
-    	{
-    		
-    	}
-    }
-    
-    @FXML
-    void changeMode(ActionEvent event) {
+	@FXML
+	void UpdateMap(ActionEvent event) throws IOException {
 
-    }
+		@SuppressWarnings("resource")
+		Socket socket = new Socket("localhost", 5555);
+		for (int i = 0; i < ImagePlaces.length; i++) {
+			if (ImagePlaces[i] != null) {
 
-    
-    @FXML
-    void initialize() throws IOException, Exception {
+				String[] get = new String[1];
+				get[0] = "getPlace";
+				get[1] = "" + ImagePlaces[i].getPlace().getSerialID();
 
-    	
-    	raninImage = new ImageView(new Image("https://i.ibb.co/JsJP3r1/Haifa1.png"));
-    	raninImage.relocate(162 , 50);
-    	mainPane.getChildren().add(raninImage);
-    	Label label= new Label();
-    	label.setText("مش دارنا");
-    	label.setFont(new Font("Arial",20));
-    	Image im= new Image("File:loc.png");
-    	ImageView newLoc = new ImageView(im);
-    	newLoc.relocate(50, 50);
-    	double aspect=0.75f;
-    	newLoc.setFitHeight(im.getHeight() * aspect);
-    	newLoc.setFitWidth(im.getWidth() * aspect);
-    	//Stage primaryStage =(Stage) mainPane.getScene().getWindow();
-    	//mainPane.getChildren().addAll(newLoc);
-    	//mainPane.getChildren().addAll(label);
+			}
+		}
 
-       	EventHandler<MouseEvent> mouseEvent = new EventHandler<MouseEvent>() {
-			
+	}
+
+	@FXML
+	void AddLocation(ActionEvent event) {
+		Mode = 0;
+		if (!NewLocation.getText().equals("") && Counter < 10) {
+			EventHandler<MouseEvent> OkEventHandler = new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					if (event.getEventType() == MouseEvent.MOUSE_CLICKED) {
+						mainPane.getChildren().remove(OK);
+						current = -1;
+					}
+					// TODO Auto-generated method stub
+
+				}
+			};
+			current = Counter;
+			Label label = new Label();
+			OK = new Button();
+			OK.setText("set");
+			OK.addEventHandler(MouseEvent.MOUSE_CLICKED, OkEventHandler);
+			label.setText(NewLocation.getText());
+			label.setFont(new Font("Quicksand", 20));
+			Image im = new Image("File:loc.png");
+			ImageView newLoc = new ImageView(im);
+			newLoc.relocate(MapImage.getLayoutX(), MapImage.getLayoutY());
+			label.relocate(MapImage.getLayoutX(), MapImage.getLayoutY());
+			double aspect = 0.75f;
+			newLoc.setFitHeight(im.getHeight() * aspect);
+			newLoc.setFitWidth(im.getWidth() * aspect);
+			newLoc.setId("imv" + Counter);
+			labels[Counter] = label;
+			locations[Counter] = newLoc;
+			Places[Counter] = new Place();
+			ImagePlaces[Counter] = new ImagePlace(Counter, locations[Counter], labels[Counter], Places[Counter],
+					(int) MapImage.getLayoutX(), (int) MapImage.getLayoutY());
+
+			mainPane.getChildren().addAll(locations[Counter]);
+			mainPane.getChildren().addAll(labels[Counter]);
+			mainPane.getChildren().addAll(OK);
+
+			EventHandler<MouseEvent> EditTouchEvent = new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					if (Mode == 1) {
+						if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+
+							CurImagePlace = new ImagePlace();
+							CurImagePlace.setImageview((ImageView) event.getSource());
+							String ID = CurImagePlace.getImageview().getId();
+							ID = ID.replace("imv", "");
+							CurImagePlace.setId(Integer.parseInt(ID));
+							CurImagePlace.setLabel(labels[CurImagePlace.getId()]);
+							CurImagePlace.setPlace(Places[CurImagePlace.getId()]);
+
+							if (event.getSource() instanceof ImageView) {
+								ImageView imView = (ImageView) event.getSource();
+								imView.relocate(event.getSceneX() - (im.getWidth() / 2) * aspect,
+										event.getSceneY() - (im.getHeight() / 2) * aspect);
+								labels[CurImagePlace.getId()].relocate(
+										event.getSceneX() - (labels[CurImagePlace.getId()].getWidth() / 2),
+										event.getSceneY() - (labels[CurImagePlace.getId()].getHeight()
+												+ (im.getHeight() / 2) * aspect));
+								CurImagePlace.setX((int) event.getSceneX());
+								CurImagePlace.setY((int) event.getSceneY());
+
+							}
+						} else if (event.getEventType() == MouseEvent.MOUSE_CLICKED) {
+							CurImagePlace = new ImagePlace();
+							CurImagePlace.setImageview((ImageView) event.getSource());
+							String ID = CurImagePlace.getImageview().getId();
+							ID = ID.replace("imv", "");
+							CurImagePlace.setId(Integer.parseInt(ID));
+							CurImagePlace.setLabel(labels[CurImagePlace.getId()]);
+							CurImagePlace.setPlace(Places[CurImagePlace.getId()]);
+
+						}
+						// TODO Auto-generated method stub
+					}
+
+				}
+			};
+
+			EventHandler<MouseEvent> mouseEvent = new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					if (current != -1) {
+						if (event.getEventType() == MouseEvent.MOUSE_CLICKED
+								|| event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+							locations[current].relocate(event.getSceneX() - (im.getWidth() / 2) * aspect,
+									event.getSceneY() - (im.getHeight() / 2) * aspect);
+							labels[current].relocate(event.getSceneX() - (labels[current].getWidth() / 2),
+									event.getSceneY() - (labels[current].getHeight() + (im.getHeight() / 2) * aspect));
+							OK.relocate(event.getSceneX() - (OK.getWidth() / 2), event.getSceneY() + (OK.getHeight()));
+							locations[current].addEventFilter(MouseEvent.MOUSE_CLICKED, EditTouchEvent);
+							locations[current].addEventFilter(MouseEvent.MOUSE_DRAGGED, EditTouchEvent);
+							ImagePlaces[current].setX((int) event.getSceneX());
+							ImagePlaces[current].setY((int) event.getSceneY());
+						}
+						// TODO Auto-generated method stub
+
+					}
+				}
+			};
+
+			mainPane.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent);
+			mainPane.addEventHandler(MouseEvent.MOUSE_DRAGGED, mouseEvent);
+			mainPane.addEventHandler(MouseEvent.MOUSE_RELEASED, mouseEvent);
+			Counter++;
+		} else {
+
+		}
+	}
+
+	@FXML
+	void changeMode(ActionEvent event) {
+		if (((Node) event.getSource()).getId().equals("btn_EditMode")) {
+			Mode = 1;
+		} else if (((Node) event.getSource()).getId().equals("btn_ViewMode")) {
+			Mode = 0;
+		}
+	}
+
+	@FXML
+	void initialize() throws IOException, Exception {
+
+		Image LinkedImage = new Image(Globals.map.getLinkCustomer());
+		raninImage = new ImageView(LinkedImage);
+		raninImage.relocate(162, 50);
+		raninImage.setFitWidth(LinkedImage.getWidth());
+		raninImage.setFitHeight(LinkedImage.getHeight());
+		mainPane.getChildren().add(raninImage);
+		Label label = new Label();
+		label.setText("مش دارنا");
+		label.setFont(new Font("Arial", 20));
+		Image im = new Image("File:loc.png");
+		ImageView newLoc = new ImageView(im);
+		newLoc.relocate(50, 50);
+		double aspect = 0.75f;
+		newLoc.setFitHeight(im.getHeight() * aspect);
+		newLoc.setFitWidth(im.getWidth() * aspect);
+		// Stage primaryStage =(Stage) mainPane.getScene().getWindow();
+		// mainPane.getChildren().addAll(newLoc);
+		// mainPane.getChildren().addAll(label);
+
+		EventHandler<MouseEvent> EditTouchEvent = new EventHandler<MouseEvent>() {
+
 			@Override
 			public void handle(MouseEvent event) {
-				// TODO Auto-generated method stub
-		    	//newLoc.relocate(event.getSceneX()-(im.getWidth()/2)*aspect, event.getSceneY()-(im.getHeight()/2)*aspect);
-		    	//label.relocate(event.getSceneX()-(label.getWidth()/2), event.getSceneY()-(label.getHeight() +(im.getHeight()/2)*aspect));
-		    	//mainPane.getChildren().addAll(newLoc);
+				if (Mode == 1) {
+					if (event.getEventType() == MouseEvent.MOUSE_CLICKED
+							|| event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+						ImageView imView = (ImageView) event.getSource();
+						imView.relocate(event.getSceneX() - (im.getWidth() / 2) * aspect,
+								event.getSceneY() - (im.getHeight() / 2) * aspect);
+						// labels[current].relocate(event.getSceneX()-(labels[current].getWidth()/2),
+						// event.getSceneY()-(labels[current].getHeight() +(im.getHeight()/2)*aspect));
+						// OK.relocate(event.getSceneX()-(OK.getWidth()/2),
+						// event.getSceneY()+(OK.getHeight()));
+					}
+					// TODO Auto-generated method stub
+				}
+
 			}
 		};
 
- 
-    	
-    }
-    
-    @FXML
-    void mouseClicked(ActionEvent event) throws IOException {
-    	
-      	Stage primaryStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-    	URL url = getClass().getResource("DefaultPage.fxml");
-			AnchorPane pane =FXMLLoader.load(url);
+		for (int i = 0; i < locations.length; i++) {
+			if (locations[i] != null) {
+				locations[i].addEventFilter(MouseEvent.MOUSE_CLICKED, EditTouchEvent);
+			}
+		}
 
-			Scene scene = new Scene(pane);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-    }
-    
-    @FXML
-    void backFunc(ActionEvent event) throws IOException {
-    	
-      	Stage primaryStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-    	URL url = getClass().getResource("DefaultPage.fxml");
-			AnchorPane pane =FXMLLoader.load(url);
+	}
 
-			Scene scene = new Scene(pane);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-    }
-    
+	@FXML
+	void mouseClicked(ActionEvent event) throws IOException {
 
-    @FXML
-    void saveImage(ActionEvent event) {
-    	  FileChooser fileChooser = new FileChooser();
+		Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		URL url = getClass().getResource("DefaultPage.fxml");
+		AnchorPane pane = FXMLLoader.load(url);
 
-          //Set extension filter
-          fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("png files (*.png)", "*.png"));
+		Scene scene = new Scene(pane);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
-          //Prompt user to select a file
-          File file = fileChooser.showSaveDialog(null);
+	@FXML
+	void backFunc(ActionEvent event) throws IOException {
 
-          if(file != null){
-              try {
-                  //Pad the capture area
-            	  WritableImage writableImage = new WritableImage((int)MapImage.getFitWidth() , (int)MapImage.getFitHeight());
-            	  //WritableImage writableImage = new WritableImage((int)2000 , (int)2000);
-                  SnapshotParameters params = new SnapshotParameters();
-                  
-                  params.setViewport(new Rectangle2D(MapImage.getLayoutX(), MapImage.getLayoutY(), MapImage.getFitWidth() ,MapImage.getFitHeight()));
-                  ((Node)mainPane).snapshot(params, writableImage);
-                  //writableImage= pixelScaleAwareCanvasSnapshot(mainPane,params,2);
-                  //((Node)mainPane).snapshot()
-                  //((Node)event.getSource()).snapshot(null, writableImage);
-                  RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
-                  //Write the snapshot to the chosen file
-                  ImageIO.write(renderedImage, "png", file);
-              }
-              catch (IOException ex)
-              { 
-            	  ex.printStackTrace(); 
-              }
-          }
-    }
-    
-    public WritableImage pixelScaleAwareCanvasSnapshot(AnchorPane node,SnapshotParameters params, double pixelScale) {
-        WritableImage writableImage = new WritableImage((int)MapImage.getFitWidth(), (int)MapImage.getFitHeight());
-        SnapshotParameters spa = params;
-        spa.setTransform(Transform.scale(pixelScale, pixelScale));
-        return node.snapshot(spa, writableImage);     
-    }
+		Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		URL url = getClass().getResource("SingleEditMap.fxml");
+		AnchorPane pane = FXMLLoader.load(url);
+
+		Scene scene = new Scene(pane);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	@FXML
+	void saveImage(ActionEvent event) {
+		FileChooser fileChooser = new FileChooser();
+
+		// Set extension filter
+		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("png files (*.png)", "*.png"));
+
+		// Prompt user to select a file
+		File file = fileChooser.showSaveDialog(null);
+
+		if (file != null) {
+			try {
+				// Pad the capture area
+				WritableImage writableImage = new WritableImage((int) raninImage.getFitWidth(),
+						(int) raninImage.getFitHeight());
+				// WritableImage writableImage = new WritableImage((int)2000 , (int)2000);
+				SnapshotParameters params = new SnapshotParameters();
+
+				params.setViewport(new Rectangle2D(raninImage.getLayoutX(), raninImage.getLayoutY(),
+						raninImage.getFitWidth(), raninImage.getFitHeight()));
+				((Node) mainPane).snapshot(params, writableImage);
+				// writableImage= pixelScaleAwareCanvasSnapshot(mainPane,params,2);
+				// ((Node)mainPane).snapshot()
+				// ((Node)event.getSource()).snapshot(null, writableImage);
+				RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+				// Write the snapshot to the chosen file
+				ImageIO.write(renderedImage, "png", file);
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	public WritableImage pixelScaleAwareCanvasSnapshot(AnchorPane node, SnapshotParameters params, double pixelScale) {
+		WritableImage writableImage = new WritableImage((int) MapImage.getFitWidth(), (int) MapImage.getFitHeight());
+		SnapshotParameters spa = params;
+		spa.setTransform(Transform.scale(pixelScale, pixelScale));
+		return node.snapshot(spa, writableImage);
+	}
 
 }
--- trgel3/src/application/Place.java
+++ trgel3/src/application/Place.java
@@ -7,62 +7,63 @@ public class Place {
 	private String PlaceName;
 	private String Description;
 	private String Classification;
-    private int Accessibility;
-    public Place()
-    {
-    	
-    }
-    public Place(String CityName, String PlaceName, String Description, String Classification, int Accessibility)
-    {
-    	this.CityName=CityName;
-    	this.PlaceName=PlaceName;
-    	this.Description=Description;
-    	this.Classification=Classification;
-    	this.Accessibility=Accessibility;    	
-    }
-    public void setCityName(String cityName) {
+	private int Accessibility;
+
+	public Place() {
+
+	}
+
+	public Place(String CityName, String PlaceName, String Description, String Classification, int Accessibility,
+			long serialID) {
+		this.CityName = CityName;
+		this.PlaceName = PlaceName;
+		this.Description = Description;
+		this.Classification = Classification;
+		this.Accessibility = Accessibility;
+		this.serialID = serialID;
+	}
+
+	public void setCityName(String cityName) {
 		CityName = cityName;
 	}
-    
-    public String getCityName() {
+
+	public String getCityName() {
 		return CityName;
 	}
-    
-    public void setPlaceName(String placeName) {
+
+	public void setPlaceName(String placeName) {
 		PlaceName = placeName;
 	}
-    
-    public String getPlaceName() {
+
+	public String getPlaceName() {
 		return PlaceName;
 	}
-	
-    public void setDescription(String description) {
+
+	public void setDescription(String description) {
 		Description = description;
 	}
-    
-    public String getDescription() {
+
+	public String getDescription() {
 		return Description;
 	}
-    
-    public void setClassification(String classification) {
+
+	public void setClassification(String classification) {
 		Classification = classification;
 	}
-    
-    public String getClassification() {
+
+	public String getClassification() {
 		return Classification;
 	}
-    
-    public void setAccessibility(int accessibility) {
+
+	public void setAccessibility(int accessibility) {
 		Accessibility = accessibility;
 	}
-    
-    public int getAccessibility() {
+
+	public int getAccessibility() {
 		return Accessibility;
 	}
 
-    public long getSerialID() {
+	public long getSerialID() {
 		return serialID;
 	}
-    
-    
 }
